be34aa07df24a94a4e0a2d11b8192c899286c4ba
Etoka Kingsley <etokakingsley@gmail.com>
2020-12-26 09:45:12 +0100

    theme setup and homepage design iimplementation

---
+++ app/src/main/java/ng/novacore/bank/ui/auth/AuthFragment.kt
@@ -0,0 +1,25 @@
+package ng.novacore.bank.ui.auth
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ng.novacore.bank.R
+
+class AuthFragment : Fragment() {
+    private lateinit var viewModel: AuthViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_auth, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(AuthViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/ng/novacore/bank/ui/auth/AuthViewModel.kt
@@ -0,0 +1,7 @@
+package ng.novacore.bank.ui.auth
+
+import androidx.lifecycle.ViewModel
+
+class AuthViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
